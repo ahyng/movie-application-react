ac96c1b5a880f98dbb84ac95843f225db2c7da0a
ahyng <ahyng1@gmail.com>
2024-12-27 17:26:18 +0900

    api 연동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import axios from 'axios';
 
 function App() {
+  const api_key = process.env.REACT_MOVIE_API_KEY;
+
+  const fetchData = async () => {
+    const options = {
+      headers : {
+          Authorization : process.env.REACT_MOVIE_AUTHORIZATION
+      }
+    }
+    const {data, status} = await axios.get('https://api.themoviedb.org/3/movie/now_playing', options)
+
+    console.log(data)
+  }
+
+  fetchData();
+
+
   return (
     <div className="App">
       <header>Movie List</header>
