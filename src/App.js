69fac0aaa1ae250fe02e71f84fdbed5b9833f030
ahyng <ahyng1@gmail.com>
2024-12-28 13:02:24 +0900

    화면에 영화 리스트 출력

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+const api_key = process.env.REACT_APP_MOVIE_API_KEY;
+
+const genre = async () => {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`
+  const response = await axios.get(url);
+  return response.data.genres
+}
+
+const fetchData = async () => {
+  const api_key = process.env.REACT_APP_MOVIE_API_KEY;
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}`
+  const response = await axios.get(url);
+  
+  const movieList = response.data.results.map((data, index) => {
+    let img_src = `https://image.tmdb.org/t/p/w300${data.poster_path}`
+     return (
+     <li key={index}>{data.title}<br/>
+      {data.release_date}<br/>
+      <img src={img_src}/>
+     </li>
+     )
+  })
+  console.log(response.data)
+  console.log(genre());
+  return <ul>{movieList}</ul>
+}
 
 function App() {
-  const api_key = process.env.REACT_MOVIE_API_KEY;
+  // const api_key = process.env.REACT_APP_MOVIE_API_KEY;
 
-  const fetchData = async () => {
-    const options = {
-      headers : {
-          Authorization : process.env.REACT_MOVIE_AUTHORIZATION
-      }
+  const [movieList, setMovieList] = useState(null);
+  useEffect(() => {
+    async function fetchMovies() {
+      const items = await fetchData();
+      setMovieList(items);
     }
-    const {data, status} = await axios.get('https://api.themoviedb.org/3/movie/now_playing', options)
-
-    console.log(data)
-  }
-
-  fetchData();
-
-
+    fetchMovies();
+  }, []);
+  
   return (
     <div className="App">
       <header>Movie List</header>
+      <div>{movieList || <p>Loading...</p>}</div>
     </div>
   );
 }
